b1257fe7014c04d41e6db572e4de26bd21546c47
Xu Haochen <xuhaochen425@gmail.com>
2016-08-10 12:42:11 +0200

    only fun left

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,74 @@
-$(document).ready(function(){
+(function ($) {
 
-	$('.count').each(function () {
-	  var $this = $(this);
-	  jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-	    duration: 1000,
-	    easing: 'swing',
-	    step: function () {
-	      $this.text(Math.ceil(this.Counter));
-	    }
-	  });
-	});
+    $.fn.visible = function (partial, hidden) {
+
+        var $t = $(this).eq(0),
+            t = $t.get(0),
+            $w = $(window),
+            viewTop = $w.scrollTop(),
+            viewBottom = viewTop + $w.height(),
+            _top = $t.offset().top,
+            _bottom = _top + $t.height(),
+            compareTop = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom,
+            clientSize = hidden === true ? t.offsetWidth * t.offsetHeight : true;
+
+        return !!clientSize && ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+    };
+
+})(jQuery);
+
+
+// Scrolling Functions
+$(window).scroll(function (event) {
+    function padNum(num) {
+        if (num < 10) {
+            return "" + num;
+        }
+        return num;
+    }
+
+    var first = 4; // Count up to 25x for first
+    var second = 23; // Count up to 4x for second
+    var third = 8;
+    function countStuffUp(points, selector, duration) { //Animate count
+        $({
+            countNumber: $(selector).text()
+        }).animate({
+            countNumber: points
+        }, {
+            duration: duration,
+            easing: 'linear',
+            step: function () {
+                $(selector).text(padNum(parseInt(this.countNumber)));
+            },
+            complete: function () {
+                $(selector).text(points);
+            }
+        });
+    }
+
+    // Output to div
+    $(".first-count").each(function (i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            countStuffUp(first, '.first-count', 1000);
+        }
+    });
+
+    // Output to div
+    $(".second-count").each(function (i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            countStuffUp(second, '.second-count', 1000);
+        }
+    });
+
+        $(".third-count").each(function (i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            countStuffUp(third, '.third-count', 1000);
+        }
+    });
 
 });
